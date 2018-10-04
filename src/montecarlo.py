39fbf4a92881c04aaadb5cb741462a5835e61d33
NeymarL <lh57506155@qq.com>
2018-10-04 13:14:43 +0800

    fix bugs in computing return G

--- src/montecarlo.py
+++ src/montecarlo.py
@@ -33,8 +33,8 @@ class MonteCarloControl(BaseController):
         '''
         return self.epsilon_greedy_action(observation, predict, return_q)
 
-    def update_q_value(self, history, rewards):
-        '''Monte-Carlo evaluation on single episode
+    def build_training_set(self, history, rewards):
+        '''Monte-Carlo evaluation
 
         Q(s, a) <- Q(s, a) + (G - Q(s, a)) / N(s, a)
 
@@ -42,66 +42,6 @@ class MonteCarloControl(BaseController):
             history = [(s1, a1), (s2, a2), ...,(sT-1, aT-1)]
             rewards = [r2, r3, ..., rT]
         '''
-        inputs, targets = self.build_training_set(history, rewards)
-        self.model.train_on_batch(inputs, targets)
-
-    def update_q_value_on_batch(self, batch_history, batch_rewards):
-        '''Monte-Carlo evaluation on batch
-        
-        Q(s, a) <- Q(s, a) + (G - Q(s, a)) / N(s, a)
-
-        Args:
-            batch_history = [[(s1, a1), (s2, a2), ...,(sT-1, aT-1)], ...]
-            batch_rewards = [[r2, r3, ..., rT], ...]
-        '''
-        x = None
-        y = None
-        N = defaultdict(int)
-        Q = defaultdict(float)
-        for i, (history, rewards) in enumerate(zip(batch_history, batch_rewards)):
-            for i, ((s, a), r) in enumerate(zip(history, rewards)):
-                s = tuple(s)
-                N[(s, a)] += 1
-                G = self.compute_return(rewards, i)
-                Q[(s, a)] += (G - Q[(s, a)]) / N[(s, a)]    # track the mean return
-
-        data = defaultdict(list)
-        for (s, a), q in Q.items():
-            data[s].append((a, q))
-
-        inputs = np.zeros((len(data), ) + self.env.observation_space.shape)
-        targets = np.zeros((len(data), self.env.action_space.n))
-        for i, (s, item) in enumerate(data.items()):
-            inputs[i] = np.array(s)
-            targets[i] = self.model.predict(np.expand_dims(inputs[i], axis=0))
-            for a, q in item:
-                targets[i, a] = q
-        self.model.train_on_batch(inputs, targets)
-
-    def update_q_value_on_batch_multithreads(self, batch_history, batch_rewards):
-        '''Multithread Monte-Carlo evaluation on batch
-        
-        Q(s, a) <- Q(s, a) + (G - Q(s, a)) / N(s, a)
-
-        Args:
-            batch_history = [[(s1, a1), (s2, a2), ...,(sT-1, aT-1)], ...]
-            batch_rewards = [[r2, r3, ..., rT], ...]
-        '''
-        x = None
-        y = None
-        with ThreadPoolExecutor(max_workers=self.max_workers) as executor:
-            futures = [executor.submit(self.build_training_set, history, rewards) 
-                       for history, rewards in zip(batch_history, batch_rewards)]
-            for future in as_completed(futures):
-                if x is None:
-                    x, y = future.result()
-                else:
-                    inputs, targets = future.result()
-                    x = np.concatenate((x, inputs), axis=0)
-                    y = np.concatenate((y, targets), axis=0)
-        self.model.train_on_batch(x, y)
-
-    def build_training_set(self, history, rewards):
         N = defaultdict(int)
         Q = defaultdict(float)
         for i, ((s, a), r) in enumerate(zip(history, rewards)):
@@ -123,8 +63,16 @@ class MonteCarloControl(BaseController):
                 targets[i, a] = q
         return (inputs, targets)
 
-    def compute_return(self, rewards, i):
+    def compute_return(self, rewards, t):
+        '''Compute return G_t
+
+        G_t = R_t+1 + 𝜸R_t+2 + 𝜸^2R_t+3 + ... + 𝜸^(T-1)R_T
+
+        Args:
+            rewards = [r_1, r_2, ...., r_T]
+            t: timestep
+        '''
         G = 0
-        for j in range(i, len(rewards)):
-            G += (self.gamma ** j) * rewards[j]
+        for idx, j in enumerate(range(t, len(rewards))):
+            G += (self.gamma ** idx) * rewards[j]
         return G
